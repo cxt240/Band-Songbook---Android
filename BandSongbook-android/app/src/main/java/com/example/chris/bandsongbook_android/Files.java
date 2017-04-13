2d70bb28486f3bd1a70e038750f15d953e70b5ef
cxt240 <cxt240@case.edu>
2017-04-12 22:06:13 -0400

    fragment listview stuff
    Files and Members now populate listview with things from the arraylist.
    each item is clickable with options

--- BandSongbook-android/app/src/main/java/com/example/chris/bandsongbook_android/Files.java
+++ BandSongbook-android/app/src/main/java/com/example/chris/bandsongbook_android/Files.java
@@ -1,13 +1,22 @@
 package com.example.chris.bandsongbook_android;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Files extends Fragment{
+
+    private ListView fileList;
 
     public Files() {}
 
@@ -17,6 +26,33 @@ public class Files extends Fragment{
         setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.fragment_member, container, false);
 
+        List<String> filenames = new ArrayList<String>();
+
+        fileList = (ListView) rootView.findViewById(R.id.file_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, filenames);
+        fileList.setAdapter(arrayAdapter);
+
+        fileList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final ArrayList parts = new ArrayList();
+                new AlertDialog.Builder(getContext())
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Play settings")
+                        .setMessage("Set the tempo and start measure")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .show();
+            }
+
+        });
         return rootView;
     }
 }
--- BandSongbook-android/app/src/main/java/com/example/chris/bandsongbook_android/Members.java
+++ BandSongbook-android/app/src/main/java/com/example/chris/bandsongbook_android/Members.java
@@ -1,14 +1,23 @@
 package com.example.chris.bandsongbook_android;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Members extends Fragment{
 
+    private ListView memberList;
     public Members() {}
 
     @Override
@@ -16,6 +25,48 @@ public class Members extends Fragment{
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.fragment_member, container, false);
+        List<String> members = new ArrayList<String>();
+        members.add("Chris");
+        members.add("Bandleader");
+
+        memberList = (ListView) rootView.findViewById(R.id.member_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, members);
+        memberList.setAdapter(arrayAdapter);
+
+        memberList.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final CharSequence[] instruments = {"Voice", "Guitar", "Drums"};
+                final ArrayList parts = new ArrayList();
+                new AlertDialog.Builder(getContext())
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Choose Parts")
+                        .setMessage("Set the part for this member")
+                        .setMultiChoiceItems(instruments, null, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                                if (isChecked) {
+                                    // If the user checked the item, add it to the selected items
+                                    parts.add(which);
+                                } else if (parts.contains(which)) {
+                                    // Else, if the item is already in the array, remove it
+                                    parts.remove(Integer.valueOf(which));
+                                }
+                            }
+                        })
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .show();
+            }
+
+        });
 
         return rootView;
     }
